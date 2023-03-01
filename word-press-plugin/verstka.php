d47f65d9ca7dc45b7889077945783f7226055d9a
Georg Andrews <daemaken@gmail.com>
2023-03-01 17:48:54 +0300

    fix

--- word-press-plugin/verstka.php
+++ word-press-plugin/verstka.php
@@ -94,13 +94,13 @@
 	add_action('wp_ajax_save_vms_article', 'save_vms_article');
 	function save_vms_article ()
 	{
-		$ds = DIRECTORY_SEPARATOR;
+        $ds = DIRECTORY_SEPARATOR;
 		$uload_dir_rel = wp_upload_dir();
 		$uload_dir_rel = parse_url($uload_dir_rel['baseurl']);
 		$uload_dir_rel = $uload_dir_rel['path'];
 		$uload_dir_abs = (substr(ABSPATH, -1) == "/" ? substr(ABSPATH, 0, -1) : ABSPATH) . $ds . 'wp-content' . $ds . 'uploads' . $ds . 'vms' . $ds;
 
-		$uploaded = array();
+        $uploaded = array();
 
 		@mkdir($uload_dir_abs . $_POST['material_id'], 0777, true);
 
@@ -115,12 +115,13 @@
 				continue;
 			}
 
+            $file_name = str_replace(['_large', '_small'], ['-large', '-small'], $file_name);
 			$file_name = strrev(implode(strrev('.'), explode(strrev('_'), strrev($file_name), 2)));
+            $file_name = str_replace(['-large', '-small'], ['_large', '_small'], $file_name);
 
 			$upd = move_uploaded_file($file_data['tmp_name'], $uload_dir_abs . $_POST['material_id'] . $ds . $file_name);
 			$uploaded[$file_name] = $upd;
 		}
-
 
 		$source = str_replace('/vms_images/', $uload_dir_rel . '/vms/' . $_POST['material_id'] . '/', $source);
 
@@ -133,7 +134,7 @@
 		if (function_exists('wp_cache_clear_cache')) {
 			wp_cache_clear_cache();
 		}
-		
+
 		echo json_encode(array(
             'status' => 1,
 			'content' => $source
@@ -171,15 +172,16 @@
 				continue;
 			}
 
-			$file_name = strrev(implode(strrev('.'), explode(strrev('_'), strrev($file_name), 2)));
+            $file_name = str_replace(['_large', '_small'], ['-large', '-small'], $file_name);
+            $file_name = strrev(implode(strrev('.'), explode(strrev('_'), strrev($file_name), 2)));
+            $file_name = str_replace(['-large', '-small'], ['_large', '_small'], $file_name);
 
-			$upd = move_uploaded_file($file_data['tmp_name'], $uload_dir_abs . $_POST['material_id'] . '_m' . $ds . $file_name);
+            $upd = move_uploaded_file($file_data['tmp_name'], $uload_dir_abs . $_POST['material_id'] . '_m' . $ds . $file_name);
 			$uploaded[$file_name] = $upd;
 		}
 
 
 		$source = str_replace('/vms_images/', $uload_dir_rel . '/vms/' . $_POST['material_id'] . '_m' . '/', $source);
-
 		$data = array(
 				'post_isvms' => 1,
 				'post_vms_content_mobile' => $source
@@ -203,7 +205,7 @@
     function save_vms_article_mode ()
 	{
 		global $wpdb, $post; //post old state & db
-		
+
 		$vms_mobile_mode = (string) $_REQUEST['vms_mobile_mode'];
         $post_id = (integer) $_REQUEST['material_id'];
 
@@ -213,7 +215,7 @@
 			);
 
 			$wpdb->update($wpdb->posts, $data, array('id' => $post_id));
-			
+
             echo json_encode(array(
                 'status' => 1
             ), JSON_NUMERIC_CHECK);
@@ -245,12 +247,12 @@
 		if (is_object($post)) {
 			$vms_content = $post->post_vms_content;
 			$is_dev_mode = get_option( 'dev_mode' ) ? true : false;
-			
+
 			?>
 			<script>
-				
+
 				window.VerstkaSDK_beforeInit = function(util) {
-					
+
 					var settings = {
 						'api_key': '<?php echo get_option( 'api_key' ); ?>',
 						'images_source': '<?php echo get_option( 'images_source' ); ?>',
@@ -294,26 +296,26 @@
 							}
 						}
 					};
-					
+
 					var xmp = util.parseHtml('<xmp name="verstka" hidden>' + JSON.stringify(settings) + '</xmp>');
-					
+
 					var textarea_desktop = util.parseHtml('<textarea id="verstka_desktop" hidden><?php echo urlencode( $post->post_vms_content); ?></textarea>');
-					
+
 					var textarea_mobile = util.parseHtml('<textarea id="verstka_mobile" hidden><?php echo urlencode( $post->post_vms_content_mobile); ?></textarea>');
-					
+
 					var post_title;
-					
+
 					var is_vms_input = util.parseHtml( '<input type="text" name="is_vms" hidden/>' );
-					
+
 					if (document.getElementById('post-body-content') !== null) {
-					
+
 						post_title = document.getElementById('titlediv');
-						
+
 						util.after( post_title, xmp );
 						util.after( post_title, textarea_desktop );
 						util.after( post_title, textarea_mobile );
 						util.after( post_title, is_vms_input );
-						
+
 						window.verstka_interface = {
 							elements: {
 								xmp: xmp,
@@ -323,32 +325,32 @@
 								default_editor: util.find('#postdivrich')
 							}
 						};
-						
+
 					}
-					
+
 				};
-				
+
 				window.verstkaActionHandler = function(action, current, prev, callback) {
-				
+
 					switch (action) {
 						case 'is_active':
 							verstka_interface.elements.default_editor.style.display = current ? 'none' : 'block';
 							verstka_interface.elements.is_vms_input.value = current ? 'true' : 'false';
-							
+
 							if ( !current ) {
 								// Fixes default editor
 								window.dispatchEvent( new Event( 'resize' ) );
 							}
-							
+
 							callback( { status: 1 } );
-							
+
 							break;
 					}
-				
+
 				};
-				
+
 			</script>
-			
+
 			<script src = "//<?php echo $is_dev_mode ? 'dev' : 'go'; ?>.verstka.org/sdk/v2.js"></script>
 			<?php
 		}
@@ -366,7 +368,7 @@
                    $content = $post->post_vms_content;
                }
            }
-           
+
         remove_filter( 'the_content', 'wpautop' );
         remove_filter( 'the_excerpt', 'wpautop' );
         remove_filter( 'the_content', 'wptexturize' );
@@ -376,15 +378,15 @@
 
 	add_action('wp_head', 'add_this_script_footer');
 	function add_this_script_footer ()
-	{ 
+	{
 		$is_dev_mode = get_option( 'dev_mode' ) ? true : false;
-		
+
 		$post = get_post();
-		
+
 		?>
-			
+
 			<?php if ($post->vms_mobile_mode == 0) {  // Mobile: off ?>
-			
+
 				<!--<meta name="viewport" content="width=1200, initial-scale=1">-->
 				<script type="text/javascript">
 					window.onVMSAPIReady = function (api) {
@@ -393,11 +395,11 @@
 						});
 					};
 				</script>
-				
+
 			<?php } ?>
-			
+
 			<?php if ($post->vms_mobile_mode == 1) {  // Mobile: auto ?>
-				
+
 				<!--<meta name="viewport" content="width=device-width, initial-scale=1">-->
 				<script type="text/javascript">
 					window.onVMSAPIReady = function (api) {
@@ -407,11 +409,11 @@
 						});
 					};
 				</script>
-				
+
 			<?php } ?>
-			
+
 			<?php if ($post->vms_mobile_mode == 2) {  // Mobile: custom ?>
-			
+
 				<!--<meta name="viewport" content="width=device-width, initial-scale=1">-->
 				<script type="text/javascript">
 					window.onVMSAPIReady = function (api) {
@@ -420,12 +422,12 @@
 						});
 					};
 				</script>
-				
+
 			<?php } ?>
-			
-			
+
+
 			<script src="//<?php echo $is_dev_mode ? 'dev' : 'go'; ?>.verstka.org/api.js" async type="text/javascript"></script>
-			
+
 			<style>
 				[data-vms-version="1"] {
 					visibility: hidden;
@@ -434,9 +436,9 @@
 					visibility: visible;
 				}
 			</style>
-		
+
 		<?php
-		
+
 	}
 
 //	add_filter('manage_edit-post_columns', 'add_is_vms_column', 4);
@@ -471,7 +473,7 @@
 		$sortable_columns['is_vms'] = 'is_vms_is_vms';
 		return $sortable_columns;
 	}
-	
+
 	add_action('pre_get_posts', 'add_column_is_vms_request');
 	function add_column_is_vms_request($query) {
 	 if (!is_admin())
@@ -486,11 +488,11 @@
 	{
 		echo '<style type="text/css">.column-is_vms{width:3%;}</style>';
 	}
-	
+
 	/*===============*/
 	/* settings page */
 	/*===============*/
-	
+
 	function addOption( $name, $value ) {
 		if ( get_option($name) ) {
 			update_option($name, $value);
@@ -500,7 +502,7 @@
 			add_option($name, $value, $deprecated, $autoload);
 		}
 	}
-	
+
 	add_action('admin_menu', 'mt_add_pages');
 	function mt_add_pages() {
 	    add_menu_page('Verstka', 'Verstka', 'edit_pages', __FILE__, 'verstka_settings_page');
@@ -511,7 +513,7 @@
 	    wp_register_style( 'verstka_settings_styles', plugins_url ( 'styles/settings.css', __FILE__ ) );
 	    wp_enqueue_style( 'verstka_settings_styles' );
 	}
-	
+
 	function verstka_settings_page()
 	{
 		$hidden_field_name = 'verstka_settings_hidden';
@@ -521,7 +523,7 @@
 			'images_source',
 			'dev_mode'
 		);
-		
+
 		if ( $_POST[ $hidden_field_name ] == 'true' ) {
 			if ( $_POST[ 'confirm' ] ) {
 				foreach ( $settings_names as $name ) {
@@ -550,48 +552,48 @@
 //				}
 //			}
 		}
-		
+
 		$settings = array();
 		foreach ( $settings_names as $name ) {
 			$settings[ $name ] = get_option( $name );
 		}
 		?>
-		
+
 		<form name="verstka_settings" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER[ 'REQUEST_URI' ] ); ?>">
 			<input type="hidden" name="<?php echo $hidden_field_name; ?>" value="true">
-			
+
 			<?php if ( !$settings[ 'email' ] && false ) { ?>
-			
+
 				<div class="verstka_settings__section verstka_settings__section--step-1">
 					<p class="verstka_settings__label">Where do we send an activation link?</p>
 					<input class="verstka_settings__input" type="text" name="email" value="<?php echo get_option( 'admin_email' ); ?>">
 					<input type="submit" name="activate" value="Get activation link"/>
 				</div>
-				
+
 			<?php } elseif ( !$settings[ 'api_key' ] ) { ?>
 				<div class="verstka_settings__section verstka_settings__section--step-2">
 					<p class="verstka_settings__label">
 						<span>API key:</span>
 						<input class="verstka_settings__input" type="text" name="api_key" value="197b094891a44993b6be96edbcdb9dbc">
 					</p>
-					
+
 					<p class="verstka_settings__label">
 						<span>Images source:</span>
 						<input class="verstka_settings__input" type="text" name="images_source" value="//<?php echo $_SERVER[ 'HTTP_HOST' ]; ?>">
 					</p>
-					
+
 					<input type="submit" name="confirm" value="Confirm"/>
 				</div>
-				
+
 			<?php } else { ?>
-				
+
 				<div class="verstka_settings__section verstka_settings__section--step-2">
 					<p class="verstka_settings__label">
 						API key: <b><?php echo $settings[ 'api_key' ]; ?></b><br>
 						Images source: <b><?php echo $settings[ 'images_source' ]; ?></b>
 					</p>
 					<input type="submit" name="reset" value="Reset"/>
-					
+
 					<?php if ( $settings[ 'dev_mode' ] == 'on' ) { ?>
 						<input type="submit" name="dev_mode_off" value="Turn off developer mode"/>
 					<?php } else { ?>
@@ -599,10 +601,10 @@
 					<?php } ?>
 					<input type="text" hidden="hidden" name="dev_mode" value="<?php echo ( $settings[ 'dev_mode' ] == 'on' ) ? 'on' : 'off' ?>">
 				</div>
-			
+
 			<?php } ?>
 		</form>
-	
+
 		<?php
 	}
 ?>
