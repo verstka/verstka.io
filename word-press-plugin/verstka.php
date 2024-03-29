fae1bfc156208a147ab5ca1e955529b248710229
Artem Legotin <arlegotin@gmail.com>
2024-03-29 15:30:07 +0100

    Fix HTML decoding

--- word-press-plugin/verstka.php
+++ word-press-plugin/verstka.php
@@ -426,9 +426,13 @@ function apply_vms_content_after($content)
 			document.querySelectorAll('article')[0].classList.add('shown');
 		};
 
+		function decodeHtml(base64) {
+		    return decodeURIComponent(atob(base64).split('').map(function(c) {return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);}).join(''))
+	      	}
+
 		var htmls = {
-			desktop: atob(`{$desktop}`),
-			mobile: atob(`{$mobile}`),
+			desktop: decodeHtml(`{$desktop}`),
+			mobile: decodeHtml(`{$mobile}`),
 		};
 		var isMobile = false;
 		var prev = null;
